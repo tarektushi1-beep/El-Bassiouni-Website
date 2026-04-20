5ec4355c29133bd97e847860d792a7e2f424f02a
Elbassiouni <elbassiouni@elbassiouni.com>
2026-04-20 22:58:06 +0200

    feat: add Navbar with desktop dropdown and mobile menu

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom'
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  default: (props: any) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    const React = require('react')
+    return React.createElement('img', { ...props, alt: props.alt || '' })
+  },
+}))
