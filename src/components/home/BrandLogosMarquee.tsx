bdcca8d44f955a5eb334cbf093d3699df440583d
Elbassiouni <elbassiouni@elbassiouni.com>
2026-04-21 20:19:30 +0200

    feat: home page fetches categories from Sanity, passes to components as props

--- src/components/home/BrandLogosMarquee.tsx
+++ src/components/home/BrandLogosMarquee.tsx
@@ -1,16 +1,18 @@
 // src/components/home/BrandLogosMarquee.tsx
-import { categories } from '@/data/categories'
+import type { SanityBrand } from '@/sanity/lib/types'
 
-const allBrands = categories.flatMap((cat) => cat.brands)
+interface BrandLogosMarqueeProps {
+  brands: SanityBrand[]
+}
 
-export default function BrandLogosMarquee() {
+export default function BrandLogosMarquee({ brands }: BrandLogosMarqueeProps) {
   return (
     <section className="bg-eb-gray py-12 overflow-hidden">
       <p className="text-center font-aspire text-xs tracking-[0.3em] uppercase text-gray-500 mb-8">
         Our Brand Portfolio
       </p>
       <div className="flex animate-marquee whitespace-nowrap">
-        {[...allBrands, ...allBrands].map((brand, i) => (
+        {[...brands, ...brands].map((brand, i) => (
           <div
             key={`${brand.slug}-${i}`}
             className="inline-flex items-center mx-12 text-gray-400 hover:text-white transition-colors"
