e15c8607725d9c2554d46b28f89ebdfde5cff28a
Elbassiouni <elbassiouni@elbassiouni.com>
2026-04-21 20:18:02 +0200

    feat: embed Sanity Studio at /studio, add SiteChrome layout wrapper

---
+++ src/app/studio/[[...tool]]/page.tsx
@@ -0,0 +1,9 @@
+// src/app/studio/[[...tool]]/page.tsx
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity.config'
+
+export const dynamic = 'force-dynamic'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
