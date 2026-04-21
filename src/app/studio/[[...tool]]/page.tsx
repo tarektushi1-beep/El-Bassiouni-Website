91d919702eab7156508c5d65b96e54ff737f65ed
Elbassiouni <elbassiouni@elbassiouni.com>
2026-04-21 21:15:46 +0200

    fix: remove embedded studio (requires React 19); use hosted sanity.studio instead

--- src/app/studio/[[...tool]]/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/studio/[[...tool]]/page.tsx
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
-
-export const dynamic = 'force-dynamic'
-
-export default function StudioPage() {
-  return <NextStudio config={config} />
-}
