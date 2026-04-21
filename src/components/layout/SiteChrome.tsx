f262a9dd6d5485dc8fa8366cfb5253a885d2520f
Elbassiouni <elbassiouni@elbassiouni.com>
2026-04-22 01:24:41 +0200

    feat: add floating language switcher with GB/EG flags

--- src/components/layout/SiteChrome.tsx
+++ src/components/layout/SiteChrome.tsx
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import WhatsAppButton from './WhatsAppButton'
+import LanguageSwitcher from '@/components/ui/LanguageSwitcher'
 
 interface SiteChromeProps {
   children: React.ReactNode
@@ -24,6 +25,7 @@ export default function SiteChrome({ children }: SiteChromeProps) {
       <main className="pt-20">{children}</main>
       <Footer />
       <WhatsAppButton />
+      <LanguageSwitcher />
     </>
   )
 }
